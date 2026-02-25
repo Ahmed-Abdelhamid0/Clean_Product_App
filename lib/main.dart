8ca4c1f10a64e35bec7a1b130b58adb3626669b9
AhmedAbdelhamed <121736377+Ahmed-Abdelhamid0@users.noreply.github.com>
2026-02-25 13:51:10 +0200

    feat: hide status and keep nav

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:product_app/core/observer/bloc_observer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  //  ToDo : ===== This Line Hide Status_Bar and Keep Navigation_Bar =====
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: [SystemUiOverlay.bottom]);
   Bloc.observer=MyBlocObserver();
   runApp(const ProductApp());
